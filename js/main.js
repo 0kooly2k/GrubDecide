87bcd6f5d35498ba6184cf16fe6ea18e123d4a6a
Casey Snepar <sneparc.gmail.com>
2016-08-17 14:47:24 -0400

    Added main functionality of food decision algorithm to site, however it is unfinished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
    	//Select Menu functionality
      $( "#mood" ).selectmenu();
    	 $( "#cuisine" ).selectmenu();
@@ -7,10 +8,11 @@ $(document).ready(function() {
    	 $("#foodType").selectmenu();
 
 //Adds an event listener when a user chooses from select menu
-$("#mood").on( "click", function() {
+
 
   //declares array of restaurants
-  var FoodPlaces= ["Delilahs", "Peter Lugers"];
+  var FoodPlaces= ["Delilahs", "Peter Lugers", "Champs Diner", "Green Symphony", "V Spot", "Dirt Candy",  "Kajitsu", "Strictly Vegetarian Restaurant", " Red Bamboo", "Vegetarian Dim Sum House", "Outpost Café",  "Le Village", "Cafe Rakka", "Zestt", "Ikraveit Foods", "Bombay Sandwich", "Andaz Indian Restaurant","Terri",
+   "Organique", "Pho Sure", "Love Mamak", "Boulevard Restaurant", "Puerto Viejo", "Tablao", "Bite", "Cafe Tarantin", "CAFÉ MEDI"];
  	
   //adds an if else function to add food place decision to div that gives user the program's decision.
  	
@@ -20,21 +22,92 @@ $("#mood").on( "click", function() {
 	 	// var dietary = $("#dietary").val();
 
   /*food decision algorithm:
-    1. USER chooses what mood they have currently
+    1. USER chooses parameters from drop down menus
     2. The program stores the data when chosen.
     3. The program uses logic to decide which restaurant should be chosen.
     4. The program displays this decision on a GrubDecide div.
 
   */
+  $("form").on('submit', function (e) {
+    // $.cookie('mood', $(this).find('[name=mood]').val());
+    // $.cookie('cuisine', $(this).find('[name=cuisine]').val());
+    // $.cookie('dietary', $(this).find('[name=dietary]').val());
+    // $.cookie('price', $(this).find('[name=price]').val());
+    // $.cookie('foodType', $(this).find('[name=foodType]').val());
+    //stop form from submitting to another page
+    e.preventDefault();
+    var mood = $("#mood").val();
+    var cuisine = $("#cuisine").val();
+    var dietary = $("#dietary").val();
+    var price = $("#price").val();
+    var foodType = $("#foodType").val();
 
- 	if($('mood').val() == "Sad"){
- 		console.log(FoodPlaces[0]);
- 		$('#foodDecision').append(FoodPlaces[0]);
- 	}else{
- 		$('#foodDecision').append(FoodPlaces[1])
- 		console.log(FoodPlaces[1])
- 	}	
+    if(mood == "Sad" && cuisine == "American" && dietary == "No preference" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[0]);
+      }else if(mood == "Happy" && cuisine == "American" && dietary == "No preference" && price == "$$$$" && foodType =="Steak"){
+        $('#foodDecision').append(FoodPlaces[1]);
+      }else if(mood == "No preference" && cuisine == "American" && dietary == "Vegan" && price == "$$" && foodType =="No preference"){
+         $('#foodDecision').append(FoodPlaces[2]);
+      }else if(mood == "Happy" && cuisine == "American" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[3]);
+      }else if(mood == "Happy" && cuisine == "American" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[4]);
+      }else if(mood == "Happy" && cuisine == "American" && dietary == "Vegan" && price == "$$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[5]);
+      }else if (mood == "Happy" && cuisine == "American" && dietary == "Vegan" && price == "$$$$" && foodType =="Sandwich"){
+         $('#foodDecision').append(FoodPlaces[6]);
+      }else if (mood == "Happy" && cuisine == "Caribbean" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+         $('#foodDecision').append(FoodPlaces[7]);
+      }else if (mood == "Happy" && cuisine == "Caribbean" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+         $('#foodDecision').append(FoodPlaces[8]);
+       }else if (mood == "Happy" && cuisine == "Vietnamese" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+         $('#foodDecision').append(FoodPlaces[9]);
+       }else if (mood == "Happy" && cuisine == "Vietnamese" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+         $('#foodDecision').append(FoodPlaces[20]);
+       }else if (mood == "Happy" && cuisine == "Vietnamese" && dietary == "Vegan" && price == "$$$" && foodType =="Sandwich"){
+         $('#foodDecision').append(FoodPlaces[21]);
+       }else if (mood == "Happy" && cuisine == "French" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[10]);
+       }else if (mood == "Happy" && cuisine == "French" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[11]);
+      }else if (mood == "Happy" && cuisine == "Greek" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[12]);
+      }else if (mood == "Happy" && cuisine == "Greek" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[13]);
+      }else if (mood == "Happy" && cuisine == "Greek" && dietary == "Vegan" && price == "$$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[14]);
+      }else if (mood == "Happy" && cuisine == "Indian" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[15]);
+      }else if (mood == "Happy" && cuisine == "Indian" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[16]);
+      }else if (mood == "Happy" && cuisine == "Indian" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[17]);
+      }else if (mood == "Happy" && cuisine == "Thai" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[18]);
+      }else if (mood == "Happy" && cuisine == "Thai" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[19]);
+      }else if (mood == "Happy" && cuisine == "Spanish" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[22]);
+      }else if (mood == "Happy" && cuisine == "Spanish" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[23]);
+      }else if (mood == "Happy" && cuisine == "Spanish" && dietary == "Vegan" && price == "$$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[24]);
+      } else if (mood == "Happy" && cuisine == "Mediterranean" && dietary == "Vegan" && price == "$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[25]);
+      }else if (mood == "Happy" && cuisine == "Mediterranean" && dietary == "Vegan" && price == "$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[26]);
+      }else if (mood == "Happy" && cuisine == "Mediterranean" && dietary == "Vegan" && price == "$$$" && foodType =="Sandwich"){
+        $('#foodDecision').append(FoodPlaces[27]);
+      };
+    
+  });
+
+  $('#reload').click(function() {
+        location.reload();
+
+  });
+ 	
 });
+
  	
  	
-});
